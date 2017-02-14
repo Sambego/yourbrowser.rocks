b15167ee0de58543b1ca28b9bad9413f08589e21
Sambego <sambellen@gmail.com>
2017-02-14 12:20:17 +0100

    Initial setup of the webpack environment

---
+++ webpack.config.babel.js
@@ -0,0 +1,58 @@
+import webpack from 'webpack';
+import path from 'path';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+const createConfig = (env = 'production') => {
+    return {
+        entry: {
+            script: './src/js/index.js',
+        },
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: '[name].js',
+        },
+        module: {
+            rules: [{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader', 'eslint-loader'],
+            }, {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: ExtractTextPlugin.extract({
+                    fallback: ['style-loader'],
+                    use: ['css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]', 'postcss-loader', 'sass-loader'],
+                }),
+            }, {
+                test: /\.(xml|html|txt|md)$/,
+                use: 'raw-loader',
+            }],
+        },
+        devtool: 'source-map',
+        plugins: [
+            new webpack.LoaderOptionsPlugin({
+                minimize: true,
+                debug: false,
+            }),
+            new ExtractTextPlugin({
+                filename: '[name].[contenthash].css',
+                allChunks: true,
+                disable: env !== 'production',
+            }),
+            new HtmlWebpackPlugin({
+                template: './src/index.html',
+            }),
+        ],
+        devServer: {
+            port: process.env.PORT || 8080,
+            host: 'localhost',
+            publicPath: '/',
+            contentBase: './src',
+            historyApiFallback: true,
+            https: true,
+        },
+    };
+};
+
+module.exports = createConfig;
